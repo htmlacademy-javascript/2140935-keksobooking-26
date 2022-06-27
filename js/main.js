fe5826d1f2745651df132a64f5ff5fcfbd5c4990
Andrew Stepanov <stepanov.management@gmail.com>
2022-06-27 14:44:05 +0500

    В main.js поправил ошибки, убрал вызов функций

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@
 function randomInteger(min,max) {
   if (min < max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else return 'Некорректные вводные';
+  } else {
+    return 'Некорректные вводные';
+  }
 }
-randomInteger(2,5);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
 function randomNonInteger(min,max,symbols) {
   if (min < max) {
     return +(Math.random() * (max - min) + min).toFixed(symbols);
-  } else return 'Некорректные вводные';
+  } else {
+    return 'Некорректные вводные';
+  }
 }
-randomNonInteger(2.1,2.5,4);
