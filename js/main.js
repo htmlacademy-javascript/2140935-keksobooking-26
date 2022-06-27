b4d2e1690dd652a5299df07929fb7132bb067968
Andrew Stepanov <stepanov.management@gmail.com>
2022-06-27 14:36:40 +0500

    В main.js убран console.log, подключен вызов функций

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ function randomInteger(min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else return 'Некорректные вводные';
 }
-console.log(randomInteger(1,5));
+randomInteger(2,5);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+
 function randomNonInteger(min,max,symbols) {
   if (min < max) {
-    return (Math.random() * (max - min) + min).toFixed(symbols);
+    return +(Math.random() * (max - min) + min).toFixed(symbols);
   } else return 'Некорректные вводные';
 }
-console.log(randomNonInteger(2,2.2,4));
+randomNonInteger(2.1,2.5,4);
