800ea3b4c9c2aacba40e625e71fd9222cbb25bee
Keks <keks@htmlacademy.ru>
2022-07-22 07:21:23 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,9 @@
+//import {generateAds} from './data.js';
+import {similarTemplateFunction} from './similar.js';
+import {formInactive, formActive} from './form.js';
+
+similarTemplateFunction(1); //порядковый номер примера объявления, выводимого в шаблоне
+formInactive();
+formActive();
+
+
