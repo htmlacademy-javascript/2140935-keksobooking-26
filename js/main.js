cc4359beac96d079f85884136b8cebd967acdb56
Andrew Stepanov <stepanov.management@gmail.com>
2022-07-25 00:27:45 +0500

    8.19. Правда или действие (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-//import {generateAds} from './data.js';
+import './form.js';
 import {similarTemplateFunction} from './similar.js';
-import {formInactive, formActive} from './form.js';
 
 similarTemplateFunction(1); //порядковый номер примера объявления, выводимого в шаблоне
-formInactive();
-formActive();
+//formInactive();
+//formActive();
 
 
