2969816f7ce5e48f31754e0cb2dc80a137a1109a
Andrew Stepanov <stepanov.management@gmail.com>
2022-07-31 22:37:19 +0500

    Первые правки по защите

--- js/form.js
+++ js/form.js
@@ -1,71 +1,144 @@
-/*formInactive
-1. Форма заполнения информации об объявлении .ad-form содержит класс ad-form--disabled;
-2. Все интерактивные элементы формы .ad-form должны быть заблокированы с помощью атрибута
-disabled, добавленного на них или на их родительские блоки fieldset. Слайдер также должен
-быть заблокирован;
-3. Форма с фильтрами .map__filters заблокирована так же, как и форма .ad-form — на форму
-добавлен специальный класс, а на её интерактивные элементы атрибуты disabled.
-*/
+import {sendData} from './api.js';
+import {successAlert, errorAlert, blockSubmitButton, unblockSubmitButton} from './utils.js';
 
-const formInactive = function() {
+const adFormElement = document.querySelector('.ad-form');
+const submitButtonElement = adFormElement.querySelector('.ad-form__submit');
+const typeFieldElement = adFormElement.querySelector('#type');
+const priceFieldElement = adFormElement.querySelector('#price');
+const roomFieldElement = adFormElement.querySelector('[name="rooms"]');
+const capacityFieldElement = adFormElement.querySelector('[name="capacity"]');
+const timeInElement = adFormElement.querySelector('#timein');
+const timeOutElement = adFormElement.querySelector('#timeout');
+const sliderElement = adFormElement.querySelector('.ad-form__slider');
+const valueElement = adFormElement.querySelector('#price');
 
-  const adForm = document.querySelector('.ad-form');
-  adForm.classList.add('ad-form--disabled');
-
-  const inputAll = adForm.querySelectorAll('input');
-  for (const inputElement of inputAll) {
-    inputElement.setAttribute('disabled', 'disabled');
-  }
-
-  const adFormFieldsetAll = adForm.querySelectorAll('fieldset');
-  for (const fieldsetElement of adFormFieldsetAll) {
-    fieldsetElement.setAttribute('disabled', 'disabled');
-  }
-
-  const mapFilters = document.querySelector('.map__filters');
-  mapFilters.classList.add('map__filters--disabled');
-
-  const selectAll = mapFilters.querySelectorAll('select');
-  for (const selectElement of selectAll) {
-    selectElement.setAttribute('disabled', 'disabled');
-  }
-
-  const mapFiltersFieldsetAll = mapFilters.querySelectorAll('fieldset');
-  for (const mapFiltersFieldset of mapFiltersFieldsetAll) {
-    mapFiltersFieldset.setAttribute('disabled', 'disabled');
-  }
-
+const MIN_AMOUNT = {
+  'Бунгало': 0,
+  'Квартира': 1000,
+  'Отель': 3000,
+  'Дом': 5000,
+  'Дворец': 10000
+};
+const ROOM_OPTION_SELECT = {
+  '1 комната': '1',
+  '2 комнаты': '2',
+  '3 комнаты': '3',
+  '100 комнат': '0'
+};
+const ROOM_OPTION = {
+  '1 комната': ['для 1 гостя'],
+  '2 комнаты': ['для 2 гостей', 'для 1 гостя'],
+  '3 комнаты': ['для 3 гостей', 'для 2 гостей', 'для 1 гостя'],
+  '100 комнат': ['не для гостей']
 };
 
-/*formActive
-В активном состоянии страница позволяет:
-1. Вносить изменения в форму и отправлять её на сервер;
-2. После загрузки данных с сервера просматривать похожие объявления на карте, фильтровать их и уточнять
-подробную информацию о них, показывая для каждого из объявлений карточку.
-*/
-const formActive = function() {
+// Валидация тайтла
+const pristine = new Pristine(adFormElement, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'ad-form__label',
+});
 
-  const adFormDisabled = document.querySelector('.ad-form');
-  adFormDisabled.classList.remove('ad-form--disabled');
+const validateTitle = (value) => value.length >= 30 && value.length <= 100;
 
-  const inputDisabledAll = document.querySelectorAll('input[disabled]');
-  for (const inputDisabled of inputDisabledAll) {
-    inputDisabled.removeAttribute('disabled');
-  }
+pristine.addValidator(
+  adFormElement.querySelector('#title'),
+  validateTitle,
+  'От 30 до 100 символов'
+);
 
-  const fieldsetDisabledAll = document.querySelectorAll('fieldset[disabled]');
-  for (const fieldsetDisabled of fieldsetDisabledAll) {
-    fieldsetDisabled.removeAttribute('disabled');
-  }
+// Меняем плейсхолдер цены
+typeFieldElement.addEventListener('change', () => {
+  const type = typeFieldElement[typeFieldElement.selectedIndex].textContent;
+  priceFieldElement.placeholder = MIN_AMOUNT[type];
+});
 
-  const mapFiltersDisabled = document.querySelector('.map__filters');
-  mapFiltersDisabled.classList.remove('map__filters--disabled');
-
-  const selectDisabledAll = document.querySelectorAll('select[disabled]');
-  for (const selectDisabled of selectDisabledAll) {
-    selectDisabled.removeAttribute('disabled');
-  }
-
+// Валидация цены
+const validatePrice = (value) => {
+  const type = adFormElement.querySelector('#type')[typeFieldElement.selectedIndex];
+  return value.length && parseInt(value, 10) >= MIN_AMOUNT[type.textContent];
 };
 
-export {formInactive, formActive};
+const getPriceErrorMessage = () => {
+  const type = adFormElement.querySelector('#type')[typeFieldElement.selectedIndex];
+  return `Минимальная цена для данного типа жилья ${MIN_AMOUNT[type.textContent]}`;
+};
+
+pristine.addValidator(priceFieldElement, validatePrice, getPriceErrorMessage);
+
+// Валидация к-ва комнат
+// сначала синхронизируем переключение вместимости при изменении комнат
+roomFieldElement.addEventListener('change', () => {
+  const room = roomFieldElement[roomFieldElement.selectedIndex].textContent;
+  capacityFieldElement.value = ROOM_OPTION_SELECT[room];
+});
+
+// валидируем
+const validateCapacity = () => ROOM_OPTION[roomFieldElement[roomFieldElement.selectedIndex].textContent].includes(capacityFieldElement[capacityFieldElement.selectedIndex].textContent);
+
+const getRoomErrorMessage = () => `
+    ${roomFieldElement[roomFieldElement.selectedIndex].textContent}
+    ${capacityFieldElement[capacityFieldElement.selectedIndex].textContent.toLowerCase()}
+    ${roomFieldElement[roomFieldElement.selectedIndex].textContent === '1 комната' ? 'невозможно  ' : 'невозможно'}
+  `;
+
+pristine.addValidator(capacityFieldElement, validateCapacity, getRoomErrorMessage);
+
+// Синхронизация заезда / выезда
+timeInElement.addEventListener('change', () => {
+  timeOutElement.value = timeInElement.value;
+});
+
+timeOutElement.addEventListener('change', () => {
+  timeInElement.value = timeOutElement.value;
+});
+
+// noUiSlider
+// Валидация слайдера
+noUiSlider.create(sliderElement,{
+  range: {
+    min: 0,
+    max: 100000,
+  },
+  start: 0,
+  step: 1,
+  connect: 'lower',
+  format: {
+    to: (value) => value.toFixed(0),
+    from: (value) => parseFloat(value),
+  },
+});
+
+sliderElement.noUiSlider.on('update', () => {
+  valueElement.value = sliderElement.noUiSlider.get();
+});
+
+valueElement.addEventListener('input', (evt) => {
+  sliderElement.noUiSlider.set(evt.target.value);
+  pristine.validate(sliderElement );
+});
+
+// submit
+adFormElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    blockSubmitButton(submitButtonElement);
+    sendData(
+      //тут функция onSuccess
+      () => {
+        successAlert();
+        unblockSubmitButton(submitButtonElement);
+        evt.target.reset();
+      },
+      //тут у нас функция onFail
+      () => {
+        errorAlert();
+        unblockSubmitButton(submitButtonElement);
+      },
+      new FormData(evt.target),
+    );
+  }
+
+});
+
+export {adFormElement};
